7e1a04c5ff2c0923b2b1edbeb9ad1e66c723861d
Davis Kregers <daviskregers@inbox.lv>
2017-12-16 14:22:59 +0200

    added bezier and subdivision objects

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@
 #include <camera.h>
 
 #define GLEW_BUILD
-#define WIDTH 1920
-#define HEIGHT 1080
+#define WIDTH 1440
+#define HEIGHT 900
 
 int main(int argc, char *argv[])
 {
@@ -20,8 +20,14 @@ int main(int argc, char *argv[])
 
     Display display(WIDTH, HEIGHT, "OpenGL");
     Shader shader( a.applicationDirPath().toStdString() + "/res/basicShader");
-    Texture texture( a.applicationDirPath().toStdString() + "/res/bricks.jpg" );
+
+    Texture bricks( a.applicationDirPath().toStdString() + "/res/bricks.jpg" );
+    Texture rust( a.applicationDirPath().toStdString() + "/res/rust.jpg" );
+    Texture rock( a.applicationDirPath().toStdString() + "/res/rock.jpg" );
+
     Transform transform;
+    Transform transformBezier;
+    Transform transformSubdiv;
 
 //    Vertex vertices[] =  {
 //        Vertex(glm::vec3(-0.5,-0.5,0), glm::vec2(0,0)),
@@ -31,11 +37,12 @@ int main(int argc, char *argv[])
 //    unsigned int indices[] = {0, 1, 2};
 
 //    Mesh mesh(vertices, sizeof(vertices)/sizeof(vertices[0]), indices, sizeof(indices)/sizeof(indices[0]) );
-    Mesh mesh2( a.applicationDirPath().toStdString() + "/res/monkey3.obj" );
-
+    Mesh monkey( a.applicationDirPath().toStdString() + "/res/monkey3.obj" );
+    Mesh bezier( a.applicationDirPath().toStdString() + "/res/bezier.obj" );
+    Mesh subdiv( a.applicationDirPath().toStdString() + "/res/subdivision.obj" );
     float counter = 0.0f;
 
-    Camera camera(glm::vec3(0,0,-10), 70.0f, (float)WIDTH/(float)HEIGHT, 0.01f, 1000.0f);
+    Camera camera(glm::vec3(0,0,-20), 120.0f, (float)WIDTH/(float)HEIGHT, 0.01f, 1000.0f);
 
     while(!display.isClosed()) {
 
@@ -49,16 +56,59 @@ int main(int argc, char *argv[])
         transform.GetRot().x = counter * 5;
         transform.GetRot().y = counter * 5;
         transform.GetRot().z = counter * 5;
-//        transform.SetScale(glm::vec3(cosCounter, cosCounter, cosCounter));
+
+        glm::vec3 scale(
+            (cosCounter < 0.5) ? 0.5 : cosCounter,
+            (cosCounter < 0.5) ? 0.5 : cosCounter,
+            (cosCounter < 0.5) ? 0.5 : cosCounter
+        );
+
+        transform.SetScale( scale );
+
+        /*
+         * Monkey
+         */
 
         shader.Bind();
-        texture.Bind(0);
+        bricks.Bind(0);
         shader.Update(transform, camera);
-//        mesh.Draw();
-        mesh2.Draw();
+        monkey.Draw();
+
+        /*
+         * Bezier
+         */
+
+        transformBezier = transform;
+
+        transformBezier.GetPos().x += 5;
+        transformBezier.GetPos().y += 5;
+        transformBezier.GetPos().z = 9;
+
+        shader.Bind();
+        rust.Bind(0);
+        shader.Update(transformBezier, camera);
+        bezier.Draw();
+
+        /*
+         * Subdivision
+         */
+
+        transformSubdiv = transform;
+
+        transformSubdiv.GetPos().x -= 5;
+        transformSubdiv.GetPos().y -= 5;
+        transformSubdiv.GetPos().z = 9;
+
+        shader.Bind();
+        rock.Bind(0);
+        shader.Update(transformSubdiv, camera);
+        subdiv.Draw();
+
+        /*
+         * Swap buffers & update counter
+         */
 
         display.SwapBuffers();
-
         counter += 0.0001f;
 
     }
